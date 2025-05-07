a765d63e7d62ef4dc38718ec393bda089cf9bd9a
akhiljain1907 <akhiljain@adobe.com>
2025-05-07 12:53:54 +0530

    expo 51 with static frameworks and new arch support

--- apps/AEPSampleAppNewArchEnabled/android/app/src/main/java/com/AEPSampleAppNewArchEnabled/MainActivity.kt
+++ apps/AEPSampleAppNewArchEnabled/android/app/src/main/java/com/AEPSampleAppNewArchEnabled/MainActivity.kt
@@ -1,23 +1,7 @@
-/*
-Copyright 2024 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
-
 package com.AEPSampleAppNewArchEnabled
-import android.app.Activity
-import android.app.Application
-import com.adobe.marketing.mobile.MobileCore
 
 import android.os.Build
 import android.os.Bundle
-import com.AEPSampleAppNewArchEnabled.BuildConfig
-import com.AEPSampleAppNewArchEnabled.R
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -33,9 +17,8 @@ class MainActivity : ReactActivity() {
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
     super.onCreate(null)
-
   }
-   
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -49,15 +32,13 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
           this,
-        BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
           object : DefaultReactActivityDelegate(
               this,
               mainComponentName,
               fabricEnabled
           ){})
   }
-
-
 
   /**
     * Align the back button behavior with Android S
